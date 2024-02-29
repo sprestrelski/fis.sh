06f230fe37ffd702705830aded0249ef4fb85e11
Samantha Prestrelski <samantha.prestrelski@gmail.com>
2024-02-28 19:29:23 -0800

    feat: add spinning fish

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 
-synthwave code was created by [victor ribeiro](https://github.com/victorqribeiro/retroSynthwave)
+synthwave code was created by [victor ribeiro](https://github.com/victorqribeiro/retroSynthwave)  
+fish image: https://www.amazon.com/Loftus-International-Gigantic-Rubber-Fish/dp/B0006GJZ72
--- src/App.js
+++ src/App.js
@@ -1,10 +1,51 @@
 import './App.css';
 import Synthwave from './Synthwave';
+
+import { ComponentMeta } from '@storybook/react';
+import React from 'react';
+import {
+  Button,
+  Frame,
+  Toolbar,
+  Window,
+  WindowContent,
+  WindowHeader,
+  MenuList, MenuListItem, Separator, styleReset 
+} from 'react95';
+import { styled, createGlobalStyle, ThemeProvider } from 'styled-components';
+
+import raspberry from 'react95/dist/themes/raspberry';
+
+/* Original Windows95 font (optional) */
+import ms_sans_serif from 'react95/dist/fonts/ms_sans_serif.woff2';
+import ms_sans_serif_bold from 'react95/dist/fonts/ms_sans_serif_bold.woff2';
+
+const GlobalStyles = createGlobalStyle`
+  ${styleReset}
+  @font-face {
+    font-family: 'ms_sans_serif';
+    src: url('${ms_sans_serif}') format('woff2');
+    font-weight: 400;
+    font-style: normal
+  }
+  @font-face {
+    font-family: 'ms_sans_serif';
+    src: url('${ms_sans_serif_bold}') format('woff2');
+    font-weight: bold;
+    font-style: normal
+  }
+  body, input, select, textarea {
+    font-family: 'ms_sans_serif';
+  }
+`;
 function App() {
   return (
     <div className="App">
       <Synthwave/>
-
+      <img id="fred" src="fred.png"></img>
+      <GlobalStyles />
+      <ThemeProvider theme={raspberry}>
+      </ThemeProvider>
     </div>
   );
 }
